e7ca88e2719d056904fb8022532e741b0ff4d12e
Oleg <krotov.om@edu.spbstu.ru>
2022-12-23 21:00:43 +0300

    Update task_1.py

--- Лабораторная 1/task_1.py
+++ Лабораторная 1/task_1.py
@@ -26,17 +26,36 @@ class Guitar:
         """
         Функция, которая проверяет, есть ли струны на гитаре
 
+        :return: Есть ли струны на гитаре
+
+        Примеры:
+        >>> guitar = Guitar(0, 12)
+        >>> guitar.is_empty_string()
+
+
         """
         ...
 
     def add_string_to_guitar(self, string: int) -> None:
 
+        """
+
+        :param string: Количество добавленных струн
+
+        :raise ValueError: Если количество струн превышает 6, то вызываем ошибку
+
+        Примеры:
+        >>> guitar = Guitar(0, 12)
+        >>> guitar.add_string_to_guitar(3)
+        """
         if not isinstance(string, (int, float)):
             raise TypeError("Добавляемые струны должны быть типа int")
         if string < 0:
             raise ValueError("Добавляемые струны должны быть положительным числом")
         ...
 
+if __name__ == "__main__":
+    print(doctest.testmod())
 
 class Concrete:
     def __init__(self, concrete_strength: float, sand_fraction: float):
@@ -61,12 +80,29 @@ class Concrete:
 
     def is_empty_sand(self) -> bool:
         """
-        Функция, которая проверяет, есть ли песок в бетону
+        Функция, которая проверяет, есть ли песок в бетоне
+
+        :return: Есть ли песок в бетоне
+
+        Примеры:
+        >>> concrete = Concrete(50, 0)
+        >>> concrete.is_empty_sand()
 
         """
         ...
 
     def add_sand_to_concrete(self, sand: float) -> None:
+
+        """
+
+        :param sand: Количество добавленного песка
+
+        :raise ValueError: Если количество песка превышает 100, то вызываем ошибку
+
+        Примеры:
+        >>> concrete = Concrete(50, 0)
+        >>> concrete.add_sand_to_concrete(15)
+        """
 
         if not isinstance(sand, (int, float)):
             raise TypeError("Добавляемый песок должны быть типа int или float")
@@ -74,10 +110,13 @@ class Concrete:
             raise ValueError("Добавляемый песок должен быть положительным числом")
         ...
 
+if __name__ == "__main__":
+    print(doctest.testmod())
+
 class Students_group:
     def __init__(self, student_quantity: int, student_age: int):
         """
-        Создание и подготовка к работе объекта "Бетон"
+        Создание и подготовка к работе объекта "Группа студентов"
         :param student_quantity: Количество студентов
         :param student_age: Средний возраст студентов
         Пример:
@@ -99,16 +138,32 @@ class Students_group:
         """
         Функция, которая проверяет, есть ли студенты
 
+        :return: Есть ли студенты в группе
+
+        Примеры:
+        >>> student = Students_group(0, 12)
+        >>> student.is_empty_student()
         """
         ...
 
     def add_student_to_group(self, student: float) -> None:
 
+        """
+
+        :param student: Количество добавленных студентов
+
+        :raise ValueError: Если количество студентов меньше 1, то вызываем ошибку
+
+        Примеры:
+        >>> student = Students_group(0, 12)
+        >>> student.add_student_to_group(10)
+        """
+
         if not isinstance(student, int):
-            raise TypeError("Добавляемыt студенты должны быть типа int")
+            raise TypeError("Добавляемые студенты должны быть типа int")
         if student < 0:
             raise ValueError("Добавляемые студенты должны быть положительным числом")
         ...
 
 if __name__ == "__main__":
-    doctest.testmod()
+    print(doctest.testmod())
